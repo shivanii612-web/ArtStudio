c8c34db4b3ebd6ba4eeb3c1e8a19a3321a9df941
Shivani <shivaniramakrishnan7@gmail.com>
2026-08-18 00:24:55 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {Provider} from "react-redux";
-import {store} from "./app/Store.js"
+import {store} from "./app/store.js"
 import {GoogleOAuthProvider} from "@react-oauth/google";
 createRoot(document.getElementById('root')).render(
   //wrapping store to all components using provider 
